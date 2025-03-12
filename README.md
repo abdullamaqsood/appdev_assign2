4ec9352b28e9c7c60149fa0f1db2aa75cb79aad6
Abdullah Maqsood <abdullahmaqsood2002@gmail.com>
2025-03-12 21:49:47 +0500

    final commit

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 Made by: Abdullah Maqsood - 24448
 
-Video Link: https://drive.google.com/file/d/1wxOy218jdWTyjkLPSev0SHThVExu1bjq/view?usp=sharing
+Video Link: https://drive.google.com/file/d/1IijCyt8D1uwuCqEHv7QNqUL0-UnpVzTr/view?usp=sharing
  
--- lib/products/widgets/category_buttons.dart
+++ lib/products/widgets/category_buttons.dart
@@ -25,7 +25,7 @@ class CategoryButtons extends StatelessWidget {
       child: Row(
         children: categoryList.map((category) {
           return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 8),
             child: ChoiceChip(
               label: Text(category),
               selected: selectedCategory == category,
