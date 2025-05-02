d5772cb0bb6993bc6aeba1648a543ef406a0bd9c
Abdullah Maqsood <abdullahmaqsood2002@gmail.com>
2025-05-02 16:25:09 +0500

    Final commit

--- lib/products/widgets/product_image.dart
+++ lib/products/widgets/product_image.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:appdev_assign2/products/api/products_provider.dart';
 
 class ProductImage extends StatelessWidget {
   final String imageUrl;
@@ -7,15 +9,27 @@ class ProductImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isOffline = Provider.of<ProductProvider>(context).isOffline;
+
     return Container(
       width: double.infinity,
       height: 250,
       decoration: BoxDecoration(
-        image: DecorationImage(
-          image: NetworkImage(imageUrl),
-          fit: BoxFit.cover,
-        ),
+        color: Colors.grey[300],
+        image: isOffline
+            ? null
+            : DecorationImage(
+                image: NetworkImage(imageUrl),
+                fit: BoxFit.cover,
+                onError: (error, stackTrace) {}, // prevents crash
+              ),
       ),
+      child: isOffline
+          ? const Center(
+              child:
+                  Icon(Icons.image_not_supported, size: 50, color: Colors.grey),
+            )
+          : null,
     );
   }
 }
